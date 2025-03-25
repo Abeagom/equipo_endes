dcaeeeb91d7de087c28302b0fbc89b0e50037888
Abeagom <beas.gomez.alberto@iescamas.es>
2025-03-25 13:15:48 +0100

    Añadidos equipoTest

--- code/equipo_trabajo/src/test/java/com/endes/entidad/EquipoTest.java
+++ code/equipo_trabajo/src/test/java/com/endes/entidad/EquipoTest.java
@@ -1,9 +1,12 @@
 package com.endes.entidad;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -49,13 +52,16 @@ class EquipoTest {
      * - Comprueba que el tamaño de la lista es 1.
      * - Verifica que el miembro añadido es el esperado.
      */
-    /*
+
     @Test
     @DisplayName("Añadir un miembro válido al equipo")
     void testAñadirMiembroValido() {
-        // Tu código aquí
+        Desarrollador d = new Desarrollador ("11111111H", "Juan", 1000.00, "Java");
+        equipo.añadirMiembro(d);
+        int resultadoEsperado=1;
+        assertEquals(resultadoEsperado, equipo.getMiembros().size(), "El tamaño no es el esperado");
     }
-    */
+
 
     /**
      * Debes implementar esta prueba para verificar que el método
@@ -67,13 +73,30 @@ class EquipoTest {
      * - Comprueba que la lista tiene el tamaño correcto.
      * - Verifica que los valores de productividad son los esperados.
      */
-    /*
+
     @Test
     @DisplayName("Listar las productividades del equipo")
     void testListarProductividades() {
-        // Tu código aquí
+        Desarrollador d = new Desarrollador ("11111111H", "Juan", 1000.00, "Java");
+        ResponsablePruebas r = new ResponsablePruebas("11111112H", "Pepe", 1000.00, 50);
+        equipo.añadirMiembro(d);
+        equipo.añadirMiembro(r);
+        
+        List<Double> productividades = equipo.listarProductividades();
+        
+        //Comprobar el tamaño de la lista
+        int tamañoEsperado = 2;
+        assertEquals(tamañoEsperado, productividades.size(), "Los tamaños no coinciden");
+        
+        //Comprobar la productividad del Desarrollador
+        double productividadDesarrollador = 1100.00;
+        assertEquals(productividadDesarrollador, productividades.get(0), "La productividad no coincide");
+        
+        //Comprobar la productividad del ResponsablePruebas
+        double productividadResponsablePruebas = 2000.00;
+        assertEquals(productividadResponsablePruebas, productividades.get(1), "La productividad no coincide");
     }
-    */
+
     /**
      * Verifica que un equipo nuevo se inicializa con la lista de miembros vacía.
      */
@@ -88,10 +111,12 @@ class EquipoTest {
      * Verifica que no se puede añadir un miembro null al equipo.
      * Debe lanzarse IllegalArgumentException con el mensaje adecuado.
      */
-    /*  @Test
+    
+    @Test
     @DisplayName("No se permite añadir un miembro null al equipo")
     void testAñadirMiembroNull() {
-          // Tu código aquí
+          String resultadoEsperado="El miembro no puede ser nulo.";
+  		  Exception exception = assertThrows(IllegalArgumentException.class, () -> equipo.añadirMiembro(null), "");
+          assertEquals(resultadoEsperado, exception.getMessage(), "El método permite añadir un miembro null");
     }
-    */
 }
